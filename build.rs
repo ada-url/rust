d22f1d078926997a5d591fbb305d3af30ce14f20
Ingvar Stepanyan <me@rreverser.com>
2023-08-18 17:05:12 +0100

    Make ada-rust compile for wasm32-wasi and -unknown
    This adds ability to compile for wasm32-wasi and wasm32-unknown-unknown targets.
    
    Both of those use WASI SDK for compilation and add necessary flags for linking C++ library.
    WASI SDK is a bit more straightforward, while wasm32-unknown-unknown requires a few hacks
    to get a pure Wasm output w/o any imports, but it works too.
    
    While changing flags in build.rs, also leveraged some new `cc` options and made it possible
    to compile on Windows via Clang instead of MSVC if user chooses to do so.

--- build.rs
+++ build.rs
@@ -11,20 +11,48 @@ fn main() {
         .file("./deps/ada.cpp")
         .include("./deps/ada.h")
         .include("./deps/ada_c.h")
-        .cpp(true);
+        .cpp(true)
+        .std("c++17");
 
+    let compile_target_arch = env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH");
     let compile_target_os = env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS");
     let compile_target_env = env::var("CARGO_CFG_TARGET_ENV").expect("CARGO_CFG_TARGET_ENV");
-    if !(compile_target_os == "windows" && compile_target_env == "msvc") {
-        build.compiler("clang++");
-        build.flag("-std=c++17");
-        #[cfg(feature = "libcpp")]
-        {
-            build.cpp_set_stdlib("c++");
-            println!("cargo:rustc-link-lib=c++");
+    // Except for Emscripten target (which emulates POSIX environment), compile to Wasm via WASI SDK
+    // which is currently the only standalone provider of stdlib for compilation of C/C++ libraries.
+    if compile_target_arch.starts_with("wasm") && compile_target_os != "emscripten" {
+        let wasi_sdk = env::var("WASI_SDK").unwrap_or_else(|_| "/opt/wasi-sdk".to_owned());
+        assert!(
+            std::path::Path::new(&wasi_sdk).exists(),
+            "WASI SDK not found at {wasi_sdk}"
+        );
+        build.compiler(format!("{wasi_sdk}/bin/clang++"));
+        println!("cargo:rustc-link-search={wasi_sdk}/share/wasi-sysroot/lib/wasm32-wasi");
+        // Wasm exceptions are new and not yet supported by WASI SDK.
+        build.flag("-fno-exceptions");
+        // WASI SDK only has libc++ available.
+        build.cpp_set_stdlib("c++");
+        // Explicitly link C++ ABI to avoid linking errors (it takes care of C++ -> C "lowering").
+        println!("cargo:rustc-link-lib=c++abi");
+        // Because Ada is a pure parsing library that doesn't need any OS syscalls,
+        // it's also possible to compile it to wasm32-unknown-unknown.
+        // This still requires WASI SDK for libc & libc++, but then we need a few hacks / overrides to get a pure Wasm w/o imports instead.
+        if compile_target_os == "unknown" {
+            build.target("wasm32-wasi");
+            println!("cargo:rustc-link-lib=c");
+            build.file("./deps/wasi_to_unknown.cpp");
         }
     } else {
-        build.flag("/std:c++17").static_crt(true);
+        if !(compile_target_os == "windows" && compile_target_env == "msvc") {
+            build.compiler("clang++");
+        }
+        if cfg!(feature = "libcpp") {
+            build.cpp_set_stdlib("c++");
+        }
+    }
+    // Note: it's possible to use Clang++ explicitly on Windows as well, so this check
+    // should be specifically for "is target compiler MSVC" and not "is target OS Windows".
+    if build.get_compiler().is_like_msvc() {
+        build.static_crt(true);
         link_args::windows! {
             unsafe {
                 no_default_lib(
---
+++ deps/wasi_to_unknown.cpp
@@ -0,0 +1,50 @@
+// Some shims for WASI symbols used by the WASI libc environment initializer,
+// but not actually required by Ada. This allows to compile Ada Rust to
+// wasm32-unknown-unknown with WASI SDK.
+
+#include <stdint.h>
+
+extern "C" {
+
+int32_t __imported_wasi_snapshot_preview1_environ_get(int32_t, int32_t) {
+  __builtin_unreachable();
+}
+
+int32_t __imported_wasi_snapshot_preview1_environ_sizes_get(int32_t, int32_t) {
+  __builtin_unreachable();
+}
+
+int32_t __imported_wasi_snapshot_preview1_fd_close(int32_t) {
+  __builtin_unreachable();
+}
+
+int32_t __imported_wasi_snapshot_preview1_fd_fdstat_get(int32_t, int32_t) {
+  __builtin_unreachable();
+}
+
+int32_t __imported_wasi_snapshot_preview1_fd_read(int32_t,
+                                                  int32_t,
+                                                  int32_t,
+                                                  int32_t) {
+  __builtin_unreachable();
+}
+
+int32_t __imported_wasi_snapshot_preview1_fd_seek(int32_t,
+                                                  int64_t,
+                                                  int32_t,
+                                                  int32_t) {
+  __builtin_unreachable();
+}
+
+int32_t __imported_wasi_snapshot_preview1_fd_write(int32_t,
+                                                   int32_t,
+                                                   int32_t,
+                                                   int32_t) {
+  __builtin_unreachable();
+}
+
+_Noreturn void __imported_wasi_snapshot_preview1_proc_exit(int32_t) {
+  __builtin_unreachable();
+}
+
+} // extern "C"
