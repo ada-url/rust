f444294e8007954ec80c91c8085f7272ce253014
khai96_ <hvksmr1996@gmail.com>
2023-09-04 01:27:52 +0700

    chore: allow users of test.sh to skip features

--- test.sh
+++ test.sh
@@ -6,11 +6,16 @@ msg() {
   echo "$@" >&2
 }
 
+extra_args=()
+if [[ -n "${SKIP_FEATURES:-}" ]]; then
+  extra_args+=(--skip "$SKIP_FEATURES")
+fi
+
 msg 'STEP: Test'
-cargo hack test --feature-powerset
+cargo hack test --feature-powerset "${extra_args[@]}"
 
 msg 'STEP: Clippy'
-cargo hack clippy --feature-powerset -- -D warnings
+cargo hack clippy --feature-powerset "${extra_args[@]}" -- -D warnings
 
 msg 'STEP: Doc'
-RUSTDOCFLAGS='-D warnings' cargo hack doc --feature-powerset
+RUSTDOCFLAGS='-D warnings' cargo hack doc "${extra_args[@]}" --feature-powerset
